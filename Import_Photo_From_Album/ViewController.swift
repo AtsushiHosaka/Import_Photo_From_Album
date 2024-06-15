a43134a5fbda9629dd9a62347bd24dddac3ee51e
Atsushi Hosaka <hosaka2917@gmail.com>
2024-06-15 11:07:09 +0900

    init

--- Import_Photo_From_Album/ViewController.swift
+++ Import_Photo_From_Album/ViewController.swift
@@ -1,19 +1,39 @@
-//
-//  ViewController.swift
-//  Import_Photo_From_Album
-//
-//  Created by 保坂篤志 on 2024/06/15.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // ここに必要な初期設定を追加できます
     }
-
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        // フォトアルバムを開く処理
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.delegate = self
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        } else {
+            // フォトアルバムが利用できない場合のエラーメッセージなど
+            let alert = UIAlertController(title: "Error", message: "Photo Library not available", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    // UIImagePickerControllerDelegateメソッド
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            imageView.image = selectedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
-
